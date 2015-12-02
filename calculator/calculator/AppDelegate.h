eb5142e6968674ec123447e633da2deb4355fbaf
卢大刚 <ludagang@ludagangdeMac-mini.local>
2015-12-02 11:13:02 +0800

    初始化仓库

---
+++ calculator/calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by 卢大刚 on 15/12/2.
+//  Copyright © 2015年 cncn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
